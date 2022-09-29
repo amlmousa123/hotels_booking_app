b221cfc08e9ffa99290447001d4fcd4a25fb90c6
Rowaa <rowaaata67@gmail.com>
2022-09-29 13:55:15 +0200

    Profile

---
+++ lib/data/profile/web_services/web_service_update.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../models/Update_info.dart';
+
+
+part 'web_service_update.g.dart';
+
+
+@RestApi(baseUrl: "http://api.mahmoudtaha.com/api/auth/")
+abstract class WebServiceUpdate {
+  factory WebServiceUpdate(Dio dio, {String baseUrl}) = _WebServiceUpdate;
+
+
+  @POST("update-info")
+  Future<Update_response> getUpdateresponse({
+    @Header("token") required String token,
+    @Field("name") required String name,
+    @Field("email") required String email,
+    @Field("image") required String image,
+  });
+
+}
